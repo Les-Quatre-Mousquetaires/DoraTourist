7094267d7d0271eeccf50bbaa1e62e6e8a0218bd
tranphuquy19 <tranphuquy19@gmail.com>
2019-11-15 17:41:07 +0700

    404

---
+++ App/dora-tourist-app/src/components/Meta/PageError404.js
@@ -0,0 +1,4 @@
+/*
+ * Created by @tranphuquy19 on 15/11/2019
+ * Email: tranphuquy19@gmail.com
+ */ 
--- App/dora-tourist-app/src/routes/AuthRoutes.js
+++ App/dora-tourist-app/src/routes/AuthRoutes.js
@@ -5,7 +5,6 @@
 import React from 'react';
 import LoginComponent from "../components/AuthenticationPage/LoginComponent";
 import RegisterComponent from "../components/AuthenticationPage/RegisterComponent";
-import App from "../components/MasterPage/App";
 import MasterPage from "../components/MasterPage/MasterPage";
 
 const authRoutes = [
